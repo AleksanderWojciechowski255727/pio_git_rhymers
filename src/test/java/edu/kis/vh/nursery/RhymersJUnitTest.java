a1b65ed5797ccef9c8d24ef20c9eb227c9e53608
Aleksander Wojciechowski <255727@edu.p.lodz.pl>
2026-04-06 13:17:15 +0200

    15. Dokonano walidacji kodu testami jednostkowymi i dodano komentarz o ich wyniku

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class RhymersJUnitTest {
-
+    // Projekt przechodzi testy jednostkowe
     @Test
     public void testCountIn() {
         DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
